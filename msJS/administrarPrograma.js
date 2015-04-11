b77071945db8a4aa0b100c26517ea1fa8b8a623f
kvcordero <ucrkevin@gmail.com>
2015-04-11 11:59:37 -0600

    borre un metodo

--- msJS/administrarPrograma.js
+++ msJS/administrarPrograma.js
@@ -23,29 +23,3 @@ function crearPrograma() {
         }
     });
 }
-
-//------------------------------------------------------------------------------------------------
-//                          Metodo que actualiza una empresa
-//------------------------------------------------------------------------------------------------
-function actulizarPrograma() {
-
-    var fechaInscripcion = $('#txtFechaInscripcion').val();
-    var idEmpresa = $("#hdfIdEmpresa").val();
-    
-
-    alert(identificacion);
-
-    var parametros = {
-        "fechaInscripcion": fechaInscripcion,
-        "idEmpresa": idEmpresa
-    };
-    $.ajax({
-        data: parametros,
-        url: '.../msReglasNegocioAccion/ProgramaReglasNegocioAccion.php',
-        type: 'post',
-        success: function (response) {
-            $("#mensaje").html(response);
-            //limpiarCamposAdministrarCuentasAhorros();
-        }
-    });
-}
